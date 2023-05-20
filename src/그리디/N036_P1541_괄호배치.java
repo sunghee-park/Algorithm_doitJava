b612eb4726a7b61a5f96b3fde97c7d3b13501988
lety <tkrhktyd@gmail.com>
2023-05-20 23:51:01 +0900

    최솟값을만드는괄호배치찾기

---
+++ src/그리디/N036_P1541_괄호배치.java
@@ -0,0 +1,27 @@
+package 그리디;
+
+import java.util.Scanner;
+
+public class N036_P1541_괄호배치 {  static int answer = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String example = sc.nextLine();
+        String[] str = example.split("-");
+        for (int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+            if (i == 0)
+                answer = answer + temp; // 제일 앞에 것만 더해주고
+            else
+                answer = answer - temp; // 뒤에 부분은 더한 값들을 빼줌
+        }
+        System.out.println(answer);
+    }
+    static int mySum(String a) { // 나눠진 그룹의 더하기 연산 수행 함수
+        int sum = 0;
+        String temp[] = a.split("[+]");
+        for (int i = 0; i < temp.length; i++) {
+            sum += Integer.parseInt(temp[i]);
+        }
+        return sum;
+    }
+}
