42161a38484dc2f9bcc7106a696805d774fc4ff3
lety <tkrhktyd@gmail.com>
2023-05-21 16:33:36 +0900

    최소공배수 구하기

---
+++ src/정수론/N042_P1934_최소공배수.java
@@ -0,0 +1,21 @@
+package 정수론;
+
+import java.util.Scanner;
+
+public class N042_P1934_최소공배수 {public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    for (int i = 0; i < t; i++) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a * b / gcd(a, b);
+        System.out.println(result);
+    }
+}
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+}
